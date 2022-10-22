9b750b7883ed05720fb0414b9a1e69600e198111
dylan <dylangthomas1@gmail.com>
2022-10-22 17:53:27 -0600

    set up

---
+++ src/components/Display.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Display() {
+    return (
+        <div className="display">
+            <h2 className="current">sdfa</h2>
+            <h3 className="prev">kjfhgklhalkj</h3>
+        </div>
+    )
+}
