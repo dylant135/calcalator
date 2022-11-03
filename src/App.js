e590579a9c55a55a96f6bb6272f868abbf0161de
dylan <dylangthomas1@gmail.com>
2022-11-03 10:18:51 -0600

    layout set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Display from './components/Display';
+import Buttons from './components/Buttons';
 
 function App() {
   return (
     <div className="App">
       <Display />
+      <Buttons />
     </div>
   );
 }
